bc9fbacb359b248f2313278e89254ff22778b37a
syleemk <syleemk@gmail.com>
2021-01-12 20:21:44 +0900

    [ADD] 생성 소멸 인터페이스 구현 테스트

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,9 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
@@ -11,6 +14,7 @@ public class NetworkClient {
     }
 
     public void setUrl(String url) {
+        System.out.println("NetworkClient.setUrl");
         this.url = url;
     }
 
@@ -20,11 +24,28 @@ public class NetworkClient {
     }
 
     public void call(String message) {
-        System.out.println("call : " + url + "message = " + message);
+        System.out.println("call : " + url + " message = " + message);
     }
 
     //서비스 종료시 호출
     public void disconnect() {
         System.out.println("close " + url);
+    }
+
+    /**
+     * 이름에서 알 수 있듯이 property들의 셋팅이 끝나면 (의존 관계들의 주입이 끝나면)
+     * 호출되는 메서드 (= 초기화 작업)
+     */
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
